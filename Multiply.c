a615321c912cecfed27738475fa8c263b4eb9464
Hasib Hasan <2220166@iub.edu.bd>
2023-04-08 14:17:14 +0600

    Assignment_01_Phitron

---
+++ Multiply.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    long a, b, sum = 0;
+    scanf("%ld %ld", &a, &b);
+    sum  = a*b;
+    printf("%ld", sum);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
